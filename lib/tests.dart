74c68887e28b0801f03cad831b8ebe186506c4b5
Nicolas Devel <nicolassabogal85@gmail.com>
2023-08-10 14:25:47 -0500

    fix test

--- lib/tests.dart
+++ lib/tests.dart
@@ -2,7 +2,6 @@ int divideFactorials(int numerator, int denominator) {
   return 1;
 }
 
-
 bool isPalindrome(String text){
   return false;
 }
@@ -12,23 +11,5 @@ List<int> sort(List<int> list){
 }
 
 List<int> sieveOfEratosthenes(int limit) {
-  List<bool> primes = List.filled(limit + 1, true);
-  primes[0] = primes[1] = false;
-
-  for (int i = 2; i * i <= limit; i++) {
-    if (primes[i]) {
-      for (int j = i * i; j <= limit; j += i) {
-        primes[j] = false;
-      }
-    }
-  }
-
-  List<int> result = [];
-  for (int i = 2; i <= limit; i++) {
-    if (primes[i]) {
-      result.add(i);
-    }
-  }
-
-  return result;
+  return  [];
 }
