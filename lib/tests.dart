4bd64bcbfb0e604c6a1494446da2a35c5f684f19
unknown <usuario1@M2-305-13-11372.utadeo.loc>
2023-08-10 20:52:01 -0500

    avance ejercicios

--- lib/tests.dart
+++ lib/tests.dart
@@ -1,13 +1,21 @@
+import 'dart:convert';
+
 int divideFactorials(int numerator, int denominator) {
   return 1;
 }
 
 bool isPalindrome(String text){
+  var convert = text.toLowerCase().split(" ").join();
+  var revConvert = convert.split("").reversed.join();
+  if(convert == revConvert){
+    return true;
+  }
   return false;
 }
 
 List<int> sort(List<int> list){
-  return [];
+  list.sort();
+  return list;
 }
 
 List<int> sieveOfEratosthenes(int limit) {
