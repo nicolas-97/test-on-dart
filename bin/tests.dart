9c4c0eacd105698f19dad494d552fe422829e30c
unknown <usuario1@M2-305-21-11380.utadeo.loc>
2023-08-10 20:52:26 -0500

    diego

--- bin/tests.dart
+++ bin/tests.dart
@@ -2,4 +2,9 @@ import 'package:tests/tests.dart';
 
 void main(List<String> arguments) {
   divideFactorials(5, 3);
+  print(divideFactorials(5, 3));
+  print(divideFactorials(10, 4));
+  print(divideFactorials(20, 10));
+  print(divideFactorials(180, 176));
 }
+ 
--- lib/tests.dart
+++ lib/tests.dart
@@ -1,15 +1,25 @@
+int factorial(int n) {
+  if (n == 0 || n == 1) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
+}
 int divideFactorials(int numerator, int denominator) {
-  return 1;
+double result = factorial(numerator) / factorial(denominator);
+return result.toInt(); 
 }
 
-bool isPalindrome(String text){
+bool isPalindrome(String text) {
   return false;
 }
 
-List<int> sort(List<int> list){
+List<int> sort(List<int> list) {
   return [];
 }
 
 List<int> sieveOfEratosthenes(int limit) {
-  return  [];
+  return [];
 }
+
+
