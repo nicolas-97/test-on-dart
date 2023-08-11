c0e4be242740e138700ee3011bf3f1cf103fd049
Felipe Chacón <felipechacon200116@gmail.com>
2023-08-10 20:58:02 -0500

    Trabajo clase

--- bin/tests.dart
+++ bin/tests.dart
@@ -2,4 +2,7 @@ import 'package:tests/tests.dart';
 
 void main(List<String> arguments) {
   divideFactorials(5, 3);
+
+  print(divideFactorials(5,3));
+
 }
--- lib/tests.dart
+++ lib/tests.dart
@@ -1,8 +1,32 @@
 int divideFactorials(int numerator, int denominator) {
-  return 1;
+  int numerador=numerator;
+  int denominador = denominator;
+  int total1=1;
+  int total2 = 1;
+
+  for (int i = numerador; i > 0; i--) {
+            total1 = i*total1;
+        }
+
+
+  for (int i = denominador; i > 0; i--) {
+            total2 = i*total2;
+        }
+
+    
+    int total3 = total1~/total2;
+  return (total3);
 }
 
+
 bool isPalindrome(String text){
+    String [] a = text.split("");
+    String[] b = text.split("");
+    Collections.reverse(Arrays.asList(a));
+    if(a.equals(b)){
+      return true;
+    }
+    
   return false;
 }
 
