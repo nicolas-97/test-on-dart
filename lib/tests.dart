a40518a272689a2cd835d732b3b62c899d1afb41
unknown <usuario1@M2-305-10-11369.utadeo.loc>
2023-08-10 21:23:07 -0500

    Daniel Santos

--- lib/tests.dart
+++ lib/tests.dart
@@ -1,6 +1,18 @@
 int divideFactorials(int numerator, int denominator) {
-  return 1;
+int num1=numerator;
+int num2=denominator;
+int factorial1 = 1;
+int factorial2 = 1;
+for(int i = num1; i>= 1; i-- ){
+    factorial1 *= i;
+  }
+for(int i = num2; i>= 1; i-- ){
+    factorial2 *= i;
+  }
+int result = factorial1 ~/ factorial2;
+  return(result);
 }
+
 
 bool isPalindrome(String text){
   return false;
