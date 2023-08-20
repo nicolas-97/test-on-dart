894273406ed1d28d68a326722f0d5e23cd537888
JuandAvendanog <141972525+JuandAvendanog@users.noreply.github.com>
2023-08-19 22:32:33 -0500

    eratostenes: muy easy

--- lib/tests.dart
+++ lib/tests.dart
@@ -13,7 +13,6 @@ bool isPalindrome(String text){
 
   text = removeDiacritics(text).toLowerCase().replaceAll(" ", "");
   String aux = text.split("").reversed.join();
-
   return text == aux;
 }
 
@@ -23,5 +22,18 @@ List<int> sort(List<int> list){
 }
 
 List<int> sieveOfEratosthenes(int limit) {
-  return  [];
+
+  List<int> numeros = List.generate(limit-1, (index) => index + 2);
+  List<int> solucion = [];
+  int aux = 0;
+
+    for(int ciclo = 0; !numeros.isEmpty; ciclo++){
+    aux = numeros[0];
+    solucion.add(aux);
+    numeros.removeWhere((item) => item%aux == 0 );
+
+  }
+
+
+  return  solucion;
 }
