3dc7ee6389708551815ed92a0cded316abee060c
Nicolas Torres D-Ghoost <nicolas.torreslop@gmail.com>
2023-08-21 20:34:05 -0500

    fix: solucion isPalindromo y error class 'FileSystemEvent' can't be extended

--- lib/tests.dart
+++ lib/tests.dart
@@ -4,20 +4,44 @@ int divideFactorials(int numerator, int denominator) {
   return 1;
 }
 
-bool isPalindrome(String text){
-  var convert = text.toLowerCase().split(" ").join();
+bool isPalindrome(String text) {
+  var convert = removeAccents(text).toLowerCase().split(" ").join();
   var revConvert = convert.split("").reversed.join();
-  if(convert == revConvert){
+  if (convert == revConvert) {
     return true;
   }
   return false;
 }
 
-List<int> sort(List<int> list){
+List<int> sort(List<int> list) {
   list.sort();
   return list;
 }
 
 List<int> sieveOfEratosthenes(int limit) {
-  return  [];
+  return [];
+}
+
+String removeAccents(String text) {
+  var asciiTable = {
+    'á': 'a',
+    'é': 'e',
+    'í': 'i',
+    'ó': 'o',
+    'ú': 'u',
+    'ñ': 'n',
+    'ç': 'c',
+  };
+
+  var newText = '';
+  for (int i = 0; i < text.length; i++) {
+    var letter = text[i];
+    if (asciiTable.containsKey(letter)) {
+      newText += asciiTable[letter]!;
+    } else {
+      newText += letter;
+    }
+  }
+
+  return newText;
 }
